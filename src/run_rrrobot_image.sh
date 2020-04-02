13d2ea98839ca4dc36e64dff8826495766a43f67
Derek Witcpalek <dwitcpa@umich.edu>
2020-04-02 17:34:59 -0400

    Added a basic functional version of a robotic arm that can be used in gazebo.

--- src/run_rrrobot_image.sh
+++ src/run_rrrobot_image.sh
@@ -1,5 +1,5 @@
 #docker run -i -h rrrobot-env -t eecs467:rrrobot bash
 
 xhost +local:docker #rrrobot-env
-docker run -it --rm --device=/dev/input/event4 --device=/dev/dri:/dev/dri -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -h rrrobot-env eecs467:rrrobot
+docker run -it --rm --device=/dev/input/event4 --device=/dev/dri:/dev/dri -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v$PWD/gazebo_models:/root/.gazebo/models -h rrrobot-env eecs467:rrrobot
 xhost -local:docker #rrrobot-env
