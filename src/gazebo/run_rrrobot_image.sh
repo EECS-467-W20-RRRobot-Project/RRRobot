bf550934208405a3efb0ca33c092418f4d609e91
Derek Witcpalek <dwitcpa@umich.edu>
2020-04-03 16:50:47 -0400

    Splits project into a docker environment folder and source code folder (for development). This change is mostly just renaming and moving files, but there are some changes to the gazebo Dockerfile to include all dependencies required for final development.

--- src/gazebo/run_rrrobot_image.sh
+++
@@ -1,7 +0,0 @@
-#docker run -i -h rrrobot-env -t eecs467:rrrobot bash
-
-#xhost +local:docker
-#docker run -it --privileged --security-opt seccomp=unconfined -v $SSH_AUTH_SOCK:/ssh-agent --env SSH_AUTH_SOCK=/ssh-agent --device=/dev/dri:/dev/dri -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --env QT_X11_NO_MITSHM=1 -h rrrobot-env eecs467:rrrobot
-#xhost -local:docker
-
-../run_rrrobot_image.sh
