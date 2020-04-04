a881191bc6d3f3036c0037cdc7503421846989e1
Derek Witcpalek <dwitcpa@umich.edu>
2020-04-03 20:57:47 -0400

    Added a joint position publisher. It currently publishes all joint angles for the robotic arm, but could be adapted to a different model

--- rrrobot_src/src/simulation_drivers/src/simulation_env/src/arm_angle_sensor_plugin.cpp
+++ rrrobot_src/src/simulation_drivers/src/simulation_env/src/arm_angle_sensor_plugin.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 
 #include "ros/ros.h"
+#include "simulation_env/arm_angles.h"
 
 using std::cout;
 using std::endl;
@@ -29,7 +30,7 @@ namespace gazebo
       }
       
       nh.reset(new ros::NodeHandle("arm_node"));
-      publisher = nh->advertise<std_msgs::Float32>("arm_positions", 1);      
+      publisher = nh->advertise<simulation_env::arm_angles>("arm_positions", 1);      
     }
 
     private:
@@ -37,18 +38,25 @@ namespace gazebo
     void onUpdate()
     {
       //cout << __func__ << endl;
-      std_msgs::Float32 msg;
-      msg.data = 0.0f;
-      gazebo::physics::BasePtr joint = model->GetChild("shoulder_joint");
+      simulation_env::arm_angles msg;
+      //msg.data = 0.0f;
+      msg.shoulder_pivot_angle = dynamic_cast<gazebo::physics::Joint*>(model->GetChild("shoulder_pivot").get())->Position();
+      msg.shoulder_joint_angle = dynamic_cast<gazebo::physics::Joint*>(model->GetChild("shoulder_joint").get())->Position();
+      msg.elbow_joint_angle = dynamic_cast<gazebo::physics::Joint*>(model->GetChild("elbow_joint").get())->Position();
+      msg.wrist_pivot_angle = dynamic_cast<gazebo::physics::Joint*>(model->GetChild("wrist_pivot").get())->Position();
+      msg.wrist_joint_angle = dynamic_cast<gazebo::physics::Joint*>(model->GetChild("wrist_joint").get())->Position();
+      msg.end_effector_pivot_angle = dynamic_cast<gazebo::physics::Joint*>(model->GetChild("end_effector_pivot").get())->Position();
+      
+      //gazebo::physics::BasePtr joint = model->GetChild("shoulder_joint");
       //cout << "Joint type: " << joint->GetType() << "joint type & EntityType::JOINT = " << (joint->GetType() & gazebo::physics::Base::EntityType::JOINT) << endl;
-      if((joint->GetType() & gazebo::physics::Base::EntityType::JOINT) != 0)
-      {
+      //if((joint->GetType() & gazebo::physics::Base::EntityType::JOINT) != 0)
+      //{
         //cout << "Got an element of type JOINT" << endl;
         //cout << "Joint Position: " << dynamic_cast<gazebo::physics::Joint*>(joint.get())->Position() << endl;
-        msg.data = dynamic_cast<gazebo::physics::Joint*>(joint.get())->Position();
+        //msg.data = dynamic_cast<gazebo::physics::Joint*>(joint.get())->Position();
 	//cout << "Position: " << msg.data << endl;
-      }
-      if(msg.data != 0.0f) publisher.publish(msg);
+      //}
+      /*if(msg.data != 0.0f) */publisher.publish(msg);
       ros::spinOnce();
     }
     physics::ModelPtr model;
