e1b84d4d164bd6a132be597cefea1afec58de036
Derek Witcpalek <dwitcpa@umich.edu>
2020-04-23 16:16:35 -0400

    main node compiles and has code for all basic functionality required. Currently untested!

---
+++ src/rrrobot_ws/src/rrrobot/include/topic_names.h
@@ -0,0 +1,7 @@
+#define CV_CLASSIFICATION_CHANNEL "/cv_model"
+#define DESIRED_GRASP_POSE_CHANNEL "/desired_grasp_pose" // pose determined from depth camera
+#define GRIPPER_STATE_CHANNEL "/ariac/arm1/gripper/state"
+#define CONVEYOR_POWER_CHANNEL "/ariac/conveyor/control"
+#define START_COMPETITON_CHANNEL "/ariac/start_competition"
+#define ARM_DESTINATION_CHANNEL "/arm_controller/destination"
+#define START_COMPETITION_CHANNEL "/ariac/start_competition"
